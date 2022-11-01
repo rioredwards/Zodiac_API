467de8f1658c9cfdb9132394d85b319d4948424e
Rio Edwards <rioredwards@gmail.com>
2022-10-31 18:39:15 -0700

    Add Horoscope data

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -29,7 +29,8 @@ describe('zodiacs routes', () => {
 
   it('/horoscopes/:sign should return horoscope detail', async () => {
     const res = await request(app).get('/horoscopes/aquarius');
-    const horoscope = 'You are going to eat chocolate cake';
+    // eslint-disable-next-line
+    const horoscope = `This is one of those days when you just can't wait for someone to stop talking so you can say something, Aquarius. It's likely you won't even wait. Don't be surprised if friction results from such strongly held opposing views. People could blow things out of proportion, since everyone is convinced that they are right. The interesting thing about this situation is that it could result in a productive time.`;
     expect(res.body).toEqual(horoscope);
   });
 });
